0420e306b387fca97e4c9652df219526ca5f0563
johnkirtley <kirtleyj16@gmail.com>
2020-07-27 10:38:56 -0700

    :ambulance: Decided To Not Incorporate Python For Now
    Will be using React & JavaScript.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Styled from 'styled-components';
 
 import axios from 'axios';
 
 const App = () => {
-	const [word, setWord] = useState('');
-
-	useEffect(() => {
-		axios
-			.get('http://localhost:5000/pattern')
-			.then((res) => {
-				setWord(res.data);
-			})
-			.catch((err) => console.log(err));
-	});
-
-	return (
-		<div>
-			<h1>{word}</h1>
-		</div>
-	);
+	return <div>Hello</div>;
 };
 
 export default App;
